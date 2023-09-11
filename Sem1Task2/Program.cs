7db20d90d1ff1e139ee828c910120ace527598bd
Andrey Smorodin <smorod1n@yandex.ru>
2023-09-11 18:27:18 +0300

    add Task2,4,6,8

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два числа: ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+if (numB > numA)
+{
+    System.Console.WriteLine("Наибольшее число: " + numB);
+    System.Console.WriteLine("Наименьшее число: " + numA);
+}
+else
+{
+    System.Console.WriteLine("Наибольшее число: " + numA);
+    System.Console.WriteLine("Наименьшее число: " + numB);
+}
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int numN = int.Parse(Console.ReadLine() ?? "0");
+int evNumN = 2;
+if (numN > 1)
+{
+    while (evNumN <= numN)
+    {
+        Console.Write(evNumN + " ");
+        evNumN = evNumN + 2;
+    }
+}
